eb1f3ddbbccc044b41caf3456c6eda58bd440dcf
Atish Kumar <b22ee015@iitj.ac.in>
2024-10-02 11:38:32 +0530

    New files added

--- contact-us/src/App.jsx
+++ contact-us/src/App.jsx
@@ -7,7 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div></div>
+    <div>
+      <div className="nav">
+        <ul>Home</ul>
+        <ul>Contact</ul>
+        <ul>About</ul>
+      </div>
+    </div>
   )
 }
 
